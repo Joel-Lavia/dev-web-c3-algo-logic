13b92ba3f69bd9420f4dc168280e16d1ed66e2bb
Joel-Lavia <joeldisidi8@gmail.com>
2024-03-04 17:08:08 +0100

    reslution first exercices

--- calculer_somme_age.js
+++ calculer_somme_age.js
@@ -3,13 +3,35 @@
 // ________________________________
 
 const personnes = [
-    { nom: 'Alice', age: 25 },
-    { nom: 'Bob', age: 30 },
-    { nom: 'Charlie', age: 20 }
-  ];
-  const ageMinimum = 25;
+  { nom: "Alice", age: 25 },
+  { nom: "Bob", age: 30 },
+  { nom: "Charlie", age: 20 },
+];
+personnes.sort((a, b) => {
+  if (a.nom < b.nom) {
+    return -1;
+  }
+  if (a.nom > b.nom) {
+    return 1;
+  }
+  return 0;
+});
+console.log(personnes);
 
+function sommeAllAge() {
+  const ageFistUser = personnes[0].age;
+  const ageSecondUser = personnes[1].age;
+  const ageThirdUser = personnes[2].age;
 
-  /**
-   * Ex: Somme des âges : 75
-   */
+  const allAge = ageFistUser + ageSecondUser + ageThirdUser;
+
+  return allAge;
+}
+
+console.log(sommeAllAge());
+
+// const ageMinimum = 25;
+
+/**
+ * Ex: Somme des âges : 75
+ */
