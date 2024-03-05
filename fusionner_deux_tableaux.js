5facee949a80df8d3f3dd1ed81421b6e0b1468a1
Joel-Lavia <joeldisidi8@gmail.com>
2024-03-05 20:38:55 +0100

    solving exercise 3

--- fusionner_deux_tableaux.js
+++ fusionner_deux_tableaux.js
@@ -3,13 +3,20 @@
 // ________________________________
 
 const tableau1 = [
-  { id: 1, nom: 'Alice' },
-  { id: 2, nom: 'Bob' }
+  { id: 1, nom: "Alice" },
+  { id: 2, nom: "Bob" },
 ];
 const tableau2 = [
-  { id: 2, nom: 'Bob' },
-  { id: 3, nom: 'Charlie' }
+  { id: 2, nom: "Bob" },
+  { id: 3, nom: "Charlie" },
 ];
+function mergeArray() {
+  const merge = tableau1.concat(tableau2);
+  const removeDup = merge.map(({ id }) => id);
+  const fil = merge.filter(({ id }, i) => !removeDup.includes(id,i+1));
+  return fil;
+}
+console.log(mergeArray());
 
 /**
  * Ex: [{ id: 1, nom: 'Alice' }, { id: 2, nom: 'Bob' }, { id: 3, nom: 'Charlie' }]
